be190009dc1cbe4c569784461f350a8a3cb5f805
Tyler Olson <tolson519@gmail.com>
2020-08-17 22:18:20 -0700

    Added random motd from config and player count

---
+++ src/dev/tylerolson/rotatingmotd/Main.java
@@ -0,0 +1,19 @@
+package dev.tylerolson.rotatingmotd;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    public static Main instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(new PingListener(), this);
+
+        for (int i = 0; i < getConfig().getStringList("motds").size(); i++) {
+            Main.instance.getLogger().info("Found MOTD: " + Main.instance.getConfig().getStringList("motds").get(i));
+        }
+    }
+}
---
+++ src/dev/tylerolson/rotatingmotd/PingListener.java
@@ -0,0 +1,46 @@
+package dev.tylerolson.rotatingmotd;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class PingListener implements Listener {
+
+    @EventHandler
+    public void onServerListPingEvent(ServerListPingEvent event) {
+        FileConfiguration config = Main.instance.getConfig();
+
+        //motd
+        int motdSize = config.getStringList("motds").size();
+        int randomMOTD = new Random().nextInt(motdSize);
+        event.setMotd(config.getStringList("motds").get(randomMOTD));
+
+        //playercount
+        String maxPlayers = config.getString("maxPlayers");
+        if (maxPlayers.contains("current")) {
+            maxPlayers = maxPlayers.replace("current", "" + event.getNumPlayers()); //replace keyword
+            String[] numbers = maxPlayers.replaceAll("\\s", "").split("\\+|(?=-)"); //make separate ints with signs
+            int result = Arrays.stream(numbers).mapToInt(Integer::parseInt).sum(); //combine ints with signs
+            event.setMaxPlayers(result);
+        } else {
+            if (isNumber(maxPlayers)) {
+                event.setMaxPlayers(Integer.parseInt(maxPlayers));
+            }
+        }
+
+        //Main.instance.getLogger().info("" + Integer.parseInt(maxPlayers));
+    }
+
+    public boolean isNumber(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+}
